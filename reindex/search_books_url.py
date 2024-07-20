a5b4471e522ebbad761cdacdc0eb5eb75409043a
MarskingxLee <shamangels@gmail.com>
2024-07-20 13:13:43 +0800

    更新文章內容： 新增20240717文章

--- reindex/search_books_url.py
+++ reindex/search_books_url.py
@@ -1,163 +1,148 @@
 import logging
+from datetime import datetime
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
 
-# 設置日誌文件
-logging.basicConfig(filename='search_book_errors.log', level=logging.DEBUG)
+# 配置日誌
+log_filename = f'search_book_errors_{datetime.now().strftime("%Y%m%d")}.log'
+logging.basicConfig(
+  filename=log_filename,
+  level=logging.DEBUG,
+  format='%(asctime)s - %(levelname)s - %(message)s',
+  datefmt='%Y-%m-%d %H:%M:%S',
+  encoding='utf-8'
+)
+
 
 def initialize_driver():
-    """初始化 Selenium WebDriver"""
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  # 如果你希望背景運行瀏覽器，移除這一行可以看到瀏覽器操作
-    driver = webdriver.Chrome(options=options)
-    logging.debug("初始化 WebDriver 完成")
-    return driver
+  options = webdriver.ChromeOptions()
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument(
+    'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+  driver = webdriver.Chrome(options=options)
+  driver.set_page_load_timeout(30)
+  logging.debug("初始化 WebDriver 完成")
+  return driver
+
 
 def close_ads(driver):
-    """關閉廣告橫幅"""
-    try:
-        close_ad_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "close_top_banner"))
-        )
-        close_ad_button.click()
-        logging.debug("廣告橫幅已關閉")
-    except TimeoutException:
-        logging.debug("未找到廣告橫幅或廣告橫幅已經關閉")
+  try:
+    close_ad_button = WebDriverWait(driver, 10).until(
+      EC.element_to_be_clickable((By.ID, "close_top_banner"))
+    )
+    close_ad_button.click()
+    logging.debug("廣告橫幅已關閉")
+  except TimeoutException:
+    logging.debug("未找到廣告橫幅或廣告橫幅已經關閉")
 
-def search_books(driver, keyword):
-    """在博客來網站上搜尋書籍並返回搜尋結果的 URL"""
-    try:
-        logging.debug("打開博客來網站")
-        driver.get("https://www.books.com.tw/")
-        close_ads(driver)
-        search_for_books(driver, keyword)
-        results = get_search_results(driver, ".mod2 .table-td")
-
-        if not results:
-            logging.debug("未找到任何結果")
-            return None, None
-
-        filtered_results = []
-        for index, result in enumerate(results):
-            if process_result(result, index):
-                filtered_results.append(result)
-
-        if not filtered_results:
-            logging.debug("未找到有效的搜尋結果")
-            return None, None
-
-        if len(filtered_results) == 1:
-            choice = 0
-            logging.debug("自動選擇唯一結果")
-        else:
-            while True:
-                try:
-                    choice = int(input("請輸入您要選擇的商品編號: ")) - 1
-                    if 0 <= choice < len(filtered_results):
-                        break
-                    else:
-                        logging.debug("無效的編號，請重新輸入")
-                except ValueError:
-                    logging.debug("請輸入數字編號")
-
-        selected_result = filtered_results[choice]
-        selected_title, product_url = get_selected_result_url(selected_result)
-
-        logging.debug(f"選擇的商品名稱: {selected_title}")
-        logging.debug(f"生成的商品 URL: {product_url}")
-        return selected_title, product_url
-
-    except (TimeoutException, NoSuchElementException, WebDriverException) as e:
-        error_message = f"Error searching book on books.com.tw: {e}"
-        logging.error(error_message)
-        return None, None
 
 def search_for_books(driver, keyword):
-    """搜尋博客來網站的書籍"""
-    try:
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "key")))
-        search_input = driver.find_element(By.ID, "key")
-        logging.debug(f"輸入關鍵字: {keyword}")
-        search_input.send_keys(keyword)
-        search_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "search_btn")))
-        driver.execute_script("arguments[0].click();", search_button)
-    except TimeoutException as e:
-        error_message = f"Timeout while searching for books: {e}"
-        logging.error(error_message)
+  try:
+    search_input = WebDriverWait(driver, 20).until(
+      EC.presence_of_element_located((By.ID, "key"))
+    )
+    search_input.clear()
+    search_input.send_keys(keyword)
 
-def get_search_results(driver, selector):
-    """獲取搜尋結果"""
-    try:
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
-        results = driver.find_elements(By.CSS_SELECTOR, selector)[:6]
-        logging.debug(f"找到 {len(results)} 個結果")
-        return results
-    except TimeoutException as e:
-        error_message = f"Timeout while getting search results on books.com.tw: {e}"
-        logging.error(error_message)
-        return []
-    except NoSuchElementException as e:
-        error_message = f"Error finding search results on books.com.tw: {e}"
-        logging.error(error_message)
-        return []
+    search_button = WebDriverWait(driver, 20).until(
+      EC.element_to_be_clickable((By.CLASS_NAME, "search_btn"))
+    )
+    search_button.click()
 
-def process_result(result, index):
-    """處理搜尋結果，顯示資訊供使用者選擇"""
-    try:
-        outer_html = result.get_attribute('outerHTML')
-        if any(skip_word in outer_html for skip_word in ["試閱", "putAjaxCart", "putAjaxNextBuy"]):
-            return False
+    # 等待搜索結果加載
+    time.sleep(5)
 
-        title_element = result.find_element(By.CSS_SELECTOR, "h4 a")
-        title = title_element.text if title_element else "No title"
-        item_id = result.get_attribute("id").split('-')[-1]
-        if "No title" in title or not item_id:
-            return False
+    # 等待搜索結果出現
+    WebDriverWait(driver, 20).until(
+      EC.presence_of_element_located((By.CSS_SELECTOR, ".table-td"))
+    )
 
-        logging.debug(f"{index + 1}: {title}")
-        return True
+    # 獲取所有搜索結果
+    book_items = driver.find_elements(By.CSS_SELECTOR, ".table-td")
 
-    except (NoSuchElementException, TimeoutException) as e:
-        error_message = f"Error finding element in result {index + 1}: {e}"
-        logging.error(error_message)
-        return False
+    if not book_items:
+      logging.warning("未找到任何書籍")
+      return []
 
-def get_selected_result_url(selected_result):
-    """獲取選擇的結果 URL"""
-    title, product_url = None, None  # 初始化變數
+    books = []
+    for i, book in enumerate(book_items[:5], 1):  # 只處理前5個結果
+      try:
+        # 獲取商品代碼
+        product_id = book.get_attribute('id').split('-')[-1]
 
-    try:
-        selected_title_element = selected_result.find_element(By.CSS_SELECTOR, "h4 a")
-        title = selected_title_element.text if selected_title_element else "No title"
-        item_id = selected_result.get_attribute("id").split('-')[-1]
-        base_url = f"https://www.books.com.tw/exep/assp.php/shamangels/products/{item_id}"
-        current_date = datetime.now().strftime('%Y%m')
-        additional_params = f"utm_source=shamangels&utm_medium=ap-books&utm_content=recommend&utm_campaign=ap-{current_date}"
-        product_url = convert_url(base_url, additional_params)
+        title_element = book.find_element(By.CSS_SELECTOR, "h4 a")
+        title = title_element.text
 
-        return title, product_url
-    except (NoSuchElementException, TimeoutException) as e:
-        error_message = f"Error getting selected result URL: {e}"
-        logging.error(error_message)
-        return None, None
+        price_element = book.find_element(By.CSS_SELECTOR, "ul.price li b:last-child")
+        price = price_element.text
 
-def convert_url(base_url, additional_params):
-    """將附加參數添加到基本 URL"""
-    if "?" in base_url:
-        return f"{base_url}&{additional_params}"
-    else:
-        return f"{base_url}?{additional_params}"
+        # 構建完整的 URL
+        url = f"https://www.books.com.tw/products/{product_id}"
+
+        books.append({"index": i, "title": title, "url": url, "price": price, "product_id": product_id})
+        logging.debug(f"找到書籍 {i}: {title}, 價格: {price}, 商品代碼: {product_id}")
+      except Exception as e:
+        logging.error(f"處理第 {i} 本書時發生錯誤: {e}")
+
+    return books
+  except Exception as e:
+    logging.error(f"搜索書籍時發生錯誤: {e}")
+    return []
+
 
 def get_books_promotion_link(keyword):
-    driver = initialize_driver()
-    title, url = search_books(driver, keyword)
+  driver = initialize_driver()
+  try:
+    driver.get("https://www.books.com.tw/")
+    close_ads(driver)
+    return search_for_books(driver, keyword)
+  except Exception as e:
+    logging.error(f"獲取書籍促銷連結時發生錯誤: {e}")
+    return []
+  finally:
     driver.quit()
-    return title, url
+
+
+def display_books(books):
+  for book in books:
+    print(f"{book['index']}. {book['title']} - 價格: {book['price']}")
+
+
+def get_user_choice(books):
+  while True:
+    try:
+      choice = int(input("請選擇一本書 (輸入編號): "))
+      if 1 <= choice <= len(books):
+        return books[choice - 1]
+      else:
+        print("無效的選擇，請重新輸入。")
+    except ValueError:
+      print("請輸入有效的數字。")
+
 
 # 示例用法
 if __name__ == "__main__":
-    books_title, books_url = get_books_promotion_link("平衡心態")
-    logging.debug(f"Books: {books_title}, URL: {books_url}")
+  keyword = "給孩子的商業啟蒙"
+  logging.info(f"開始搜索關鍵字: {keyword}")
+  books = get_books_promotion_link(keyword)
+  if books:
+    print(f"找到 {len(books)} 本相關書籍:")
+    display_books(books)
+    selected_book = get_user_choice(books)
+    logging.info(
+      f"選擇的書籍: {selected_book['title']}, URL: {selected_book['url']}, 價格: {selected_book['price']}, 商品代碼: {selected_book['product_id']}")
+    print(f"您選擇的書籍是: {selected_book['title']}")
+    print(f"URL: {selected_book['url']}")
+    print(f"價格: {selected_book['price']}")
+    print(f"商品代碼: {selected_book['product_id']}")
+  else:
+    logging.warning("未能找到書籍資訊")
+    print("抱歉，未找到相關書籍。")
+  logging.info("搜索完成")
