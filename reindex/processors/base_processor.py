1a26e2725ec4678b5d4f34c49433a7d71e886560
MarskingxLee <shamangels@gmail.com>
2024-07-24 08:35:32 +0800

    新增文章 20240723
    修正程式內容
    reindex 修正財務規劃與心態的類別

--- reindex/processors/base_processor.py
+++ reindex/processors/base_processor.py
@@ -11,6 +11,8 @@ class BaseProcessor:
         raise NotImplementedError("Each processor must implement the process method.")
 
     def write_file(self, content):
-        with open(self.file_path, 'w', encoding='utf-8') as file:
-            file.write(content)
-        logging.info(f"成功寫入文件: {self.file_path}")
+        try:
+            with open(self.file_path, 'w', encoding='utf-8') as file:
+                file.write(content)
+        except Exception as e:
+            logging.error(f"寫入文件時發生錯誤: {e}")
